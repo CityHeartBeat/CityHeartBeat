d0230058dccc8abe9b52cae1dead57ea9dc09be2
ninguchi <kewalin.sk@gmail.com>
2016-04-12 16:16:46 +0700

    initial commit

---
+++ Pods/Target Support Files/INTUAnimationEngine/INTUAnimationEngine-umbrella.h
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+#import "INTUAnimationEngine.h"
+#import "INTUAnimationEngineDefines.h"
+#import "INTUEasingFunctions.h"
+#import "INTUInterpolationFunctions.h"
+#import "INTUSpringSolver.h"
+#import "INTUVector.h"
+
+FOUNDATION_EXPORT double INTUAnimationEngineVersionNumber;
+FOUNDATION_EXPORT const unsigned char INTUAnimationEngineVersionString[];
+
